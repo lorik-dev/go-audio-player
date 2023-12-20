05dc382171e745bfb1abe0af6fd8bf08b464a1bb
Lorik <zlandriax@gmail.com>
2023-12-20 20:42:42 +0000

    Media controls
    Add media controls
    - Pause
    - Loop

--- go-music-player.go
+++ go-music-player.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/gopxl/beep"
@@ -16,12 +17,50 @@ import (
 
 type Ctrl struct {
 	Streamer beep.StreamSeekCloser
+	Format   beep.Format
 	Paused   bool
+	Loop     bool
 }
 
 func check(e error) {
 	if e != nil {
 		log.Fatal(e)
+	}
+}
+
+func returnPosition(playInstance Ctrl) (int, int, int) {
+	//Returns current position in track. Returns three separate ints: hours, minutes, seconds.
+	position := playInstance.Streamer.Position()
+	timeInSeconds := float64(position) / float64(playInstance.Format.SampleRate)
+
+	//Convert time in seconds to "HH:MM:SS" format
+	duration := time.Duration(timeInSeconds) * time.Second
+	return int(duration.Hours()), int(duration.Minutes()) % 60, int(duration.Seconds()) % 60
+}
+
+// Prints current playback, including time and options
+func printPlaybackStatus(playInstance Ctrl) {
+	// Clear terminal
+	fmt.Print("\033[H\033[2J")
+
+	// Retrieve playback position and print
+	hours, minutes, seconds := returnPosition(playInstance)
+	fmt.Printf("Time: %02d:%02d:%02d\n", hours, minutes, seconds)
+
+	// Display options
+	fmt.Println("1: Pause/Resume | 2: Loop")
+	printPlaybackOptions(playInstance)
+}
+
+func printPlaybackOptions(playInstance Ctrl) {
+	fmt.Printf("\n")
+	switch playInstance.Paused {
+	case true:
+		fmt.Printf("| PAUSED |\n")
+	}
+	switch playInstance.Loop {
+	case true:
+		fmt.Printf("| LOOP |")
 	}
 }
 
@@ -50,26 +89,37 @@ func readAudio(fileArg string) (beep.StreamSeekCloser, beep.Format) {
 	return streamerIn, formatIn
 }
 
-func playAudio(streamer beep.StreamSeekCloser, format beep.Format) (beep.StreamSeekCloser, chan bool) {
+func playAudio(playInstance Ctrl) chan bool {
 
-	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
-
+	speaker.Init(playInstance.Format.SampleRate, playInstance.Format.SampleRate.N(time.Second/10))
 	done := make(chan bool)
-	fmt.Println("Playing...")
-	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+	printPlaybackStatus(playInstance)
+	speaker.Play(beep.Seq(playInstance.Streamer, beep.Callback(func() {
 		done <- true
 	})))
-	return streamer, done
+	return done
 }
 
-func readInput(playInstance Ctrl, format beep.Format, done chan bool) {
+func startPlayback(argsWithProg []string, preLoop bool) (Ctrl, chan bool) {
+	streamerRead, formatRead := readAudio(argsWithProg[1])
+	playInstance := new(Ctrl)
+	playInstance.Streamer = streamerRead
+	playInstance.Format = formatRead
+
+	// Checks if to keep looping from previous instance
+	playInstance.Paused = false
+
+	playInstance.Loop = preLoop
+	return *playInstance, playAudio(*playInstance)
+}
+
+func readInput(playInstance Ctrl, format beep.Format, done chan bool, argsWithProg []string) Ctrl {
 	ch := make(chan string)
 	go func(ch chan string) {
 		reader := bufio.NewReader(os.Stdin)
 		for {
 			s, err := reader.ReadString('\n')
 			if err != nil { // Maybe log non io.EOF errors, if you want
-				fmt.Println("closing")
 				close(ch)
 				return
 			}
@@ -83,18 +133,29 @@ stdinloop:
 			if !ok {
 				break stdinloop
 			} else {
-				fmt.Println(stdin)
-				if stdin == "1" {
+				if strings.TrimSpace(stdin) == "1" {
 					switch playInstance.Paused {
 					case false:
 						speaker.Lock()
 						playInstance.Paused = true
-						fmt.Println("Paused")
 					case true:
 						speaker.Unlock()
 						playInstance.Paused = false
-						fmt.Println("Resume")
 					}
+					printPlaybackStatus(playInstance)
+				} else {
+					printPlaybackStatus(playInstance)
+				}
+				if strings.TrimSpace(stdin) == "2" {
+					switch playInstance.Loop {
+					case false:
+						playInstance.Loop = true
+					case true:
+						playInstance.Loop = false
+					}
+					printPlaybackStatus(playInstance)
+				} else {
+					printPlaybackStatus(playInstance)
 				}
 			}
 		case <-time.After(1 * time.Second):
@@ -102,26 +163,41 @@ stdinloop:
 			select {
 			case ok := <-done:
 				if ok {
-					fmt.Println("Playback finished.")
-					return
+					// Return updated playInstance
+					return playInstance
 				} else {
 					log.Fatal("Channel closed.")
 				}
 			default:
-				fmt.Print("\033[H\033[2J")
-				position := playInstance.Streamer.Position()
-				timeInSeconds := float64(position) / float64(format.SampleRate)
+				if !playInstance.Paused {
+					printPlaybackStatus(playInstance)
+				}
+			}
 
-				//Convert time in seconds to "HH:MM:SS" format
-				duration := time.Duration(timeInSeconds) * time.Second
-				hours := int(duration.Hours())
-				minutes := int(duration.Minutes()) % 60
-				seconds := int(duration.Seconds()) % 60
-				fmt.Printf("Time: %02d:%02d:%02d\n", hours, minutes, seconds)
+		}
+	}
+	return playInstance
+}
+
+func playbackLoop(argsWithProg []string, preLoop bool) Ctrl {
+	playInstance, done := startPlayback(argsWithProg, preLoop)
+
+	defer playInstance.Streamer.Close()
+	for {
+		select {
+		case <-done:
+		default:
+			switch playInstance.Paused {
+			case false:
+				playInstance = readInput(playInstance, playInstance.Format, done, argsWithProg)
+				return playInstance
+			case true:
+				playInstance = readInput(playInstance, playInstance.Format, done, argsWithProg)
+				return playInstance
+			default:
 			}
 		}
 	}
-	fmt.Println("Done, stdin must be closed")
 }
 
 func main() {
@@ -131,49 +207,8 @@ func main() {
 	if len(argsWithProg) == 1 || len(argsWithProg) > 2 {
 		log.Fatal("Improper arguments: ./go-music-player.exe [file]")
 	}
-
-	streamerRead, formatRead := readAudio(argsWithProg[1])
-	streamer, done := playAudio(streamerRead, formatRead)
-	playInstance := new(Ctrl)
-	playInstance.Streamer = streamer
-	playInstance.Paused = false
-
-	defer playInstance.Streamer.Close()
-	for {
-		select {
-		case <-done:
-			fmt.Println("Playback finished")
-			return
-		default:
-			switch playInstance.Paused {
-			case false:
-				//fmt.Printf("%d | Press 1 to pause at any time.", playInstance.Streamer.Position())
-				/*reader := bufio.NewReader(os.Stdin)
-				fmt.Print("-> ")
-
-				text, _ := reader.ReadString('\n')
-				text = strings.Replace(text, "\r\n", "", -1)
-				if strings.Compare("1", text) == 0 {
-					speaker.Lock()
-					playInstance.Paused = true
-				}*/
-				readInput(*playInstance, formatRead, done)
-				os.Exit(0)
-			case true:
-				/*fmt.Printf("%d | Press 1 to resume at any time.", playInstance.Streamer.Position())
-				reader := bufio.NewReader(os.Stdin)
-				fmt.Print("-> ")
-
-				text, _ := reader.ReadString('\n')
-				text = strings.Replace(text, "\r\n", "", -1)
-				if strings.Compare("1", text) == 0 {
-					speaker.Unlock()
-					playInstance.Paused = false
-				}*/
-				readInput(*playInstance, formatRead, done)
-				os.Exit(0)
-			default:
-			}
-		}
+	playInstance := playbackLoop(argsWithProg, false)
+	for playInstance.Loop {
+		playInstance = playbackLoop(argsWithProg, true)
 	}
 }
