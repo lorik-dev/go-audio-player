7880530b0d29b36d4edc9e485ceef26714a5182b
Lorik <zlandriax@gmail.com>
2023-12-22 23:33:50 +0000

    Fix changes

--- go-audio-player.go
+++ go-audio-player.go
@@ -9,18 +9,30 @@ import (
 	"strings"
 	"time"
 
+	"github.com/dhowden/tag"
 	"github.com/gopxl/beep"
 	"github.com/gopxl/beep/flac"
 	"github.com/gopxl/beep/mp3"
 	"github.com/gopxl/beep/speaker"
+	"github.com/gopxl/beep/vorbis"
+	"github.com/gopxl/beep/wav"
+
+	wavmeta "github.com/go-audio/wav"
 )
 
 type Ctrl struct {
-	fileName string
-	Streamer beep.StreamSeekCloser
-	Format   beep.Format
-	Paused   bool
-	Loop     bool
+	fileName      string
+	fileExtension string
+	metadata      tag.Metadata
+	Streamer      beep.StreamSeekCloser
+	Format        beep.Format
+	Paused        bool
+	Loop          bool
+}
+
+type metadata struct {
+	nonWavMetadata tag.Metadata
+	WavMetadata    *wavmeta.Decoder
 }
 
 func check(e error) {
@@ -40,10 +52,20 @@ func returnPosition(playInstance Ctrl) (int, int, int) {
 }
 
 // Prints current playback, including time and options
-func printPlaybackStatus(playInstance Ctrl) {
+func printPlaybackStatus(playInstance Ctrl, metadataInstance metadata) {
 	// Clear terminal
 	fmt.Print("\033[H\033[2J")
-	fmt.Println(playInstance.fileName + "\n")
+
+	switch playInstance.fileExtension {
+	case ".wav":
+		fmt.Printf("%s\n%s\n", metadataInstance.WavMetadata.Metadata.Title, metadataInstance.WavMetadata.Metadata.Artist)
+		fmt.Printf("WAV %02dKHz/%02dbit", metadataInstance.WavMetadata.SampleRate/1000, metadataInstance.WavMetadata.BitDepth)
+	default:
+		fmt.Printf("%s\n%s\n", metadataInstance.nonWavMetadata.Title(), metadataInstance.nonWavMetadata.Composer())
+		fmt.Printf("%s %02dKHz\n", metadataInstance.nonWavMetadata.FileType(), playInstance.Format.SampleRate/1000)
+	}
+	fmt.Print("\n\n")
+
 	// Retrieve playback position and print
 	hours, minutes, seconds := returnPosition(playInstance)
 	fmt.Printf("Time: %02d:%02d:%02d\n", hours, minutes, seconds)
@@ -65,7 +87,7 @@ func printPlaybackOptions(playInstance Ctrl) {
 	}
 }
 
-func readAudio(fileArg string) (string, beep.StreamSeekCloser, beep.Format) {
+func readAudio(fileArg string) (*os.File, string, string, beep.StreamSeekCloser, beep.Format) {
 	file, fileErr := os.Open(fileArg)
 	check(fileErr)
 
@@ -76,6 +98,8 @@ func readAudio(fileArg string) (string, beep.StreamSeekCloser, beep.Format) {
 	var formatIn beep.Format
 	var decodeErr error
 
+	file, fileErr = os.Open(fileArg)
+
 	switch fileExtension {
 	case ".mp3":
 		strm, frm, err := mp3.Decode(file)
@@ -83,40 +107,72 @@ func readAudio(fileArg string) (string, beep.StreamSeekCloser, beep.Format) {
 	case ".flac":
 		strm, frm, err := flac.Decode(file)
 		streamerIn, formatIn, decodeErr = strm, frm, err
+	case ".ogg":
+		strm, frm, err := vorbis.Decode(file)
+		streamerIn, formatIn, decodeErr = strm, frm, err
+	case ".wav":
+		strm, frm, err := wav.Decode(file)
+		streamerIn, formatIn, decodeErr = strm, frm, err
 	default:
-		log.Fatal("Invalid file type.")
+		panic("FATAL: Invalid file type.")
 	}
 
 	check(decodeErr)
-	return fileName, streamerIn, formatIn
+	return file, fileExtension, fileName, streamerIn, formatIn
 }
 
-func playAudio(playInstance Ctrl) chan bool {
+func readNonWavMetadata(fileRead os.File) tag.Metadata {
+	file := os.File(fileRead)
+	metadata, err := tag.ReadFrom(&file)
+	check(err)
 
+	return metadata
+}
+
+func playAudio(playInstance Ctrl, metadataInstance metadata) chan bool {
 	speaker.Init(playInstance.Format.SampleRate, playInstance.Format.SampleRate.N(time.Second/10))
+	printPlaybackStatus(playInstance, metadataInstance)
 	done := make(chan bool)
-	printPlaybackStatus(playInstance)
 	speaker.Play(beep.Seq(playInstance.Streamer, beep.Callback(func() {
 		done <- true
 	})))
 	return done
 }
 
-func startPlayback(argsWithProg []string, preLoop bool) (Ctrl, chan bool) {
-	fileNameRead, streamerRead, formatRead := readAudio(argsWithProg[1])
+func startPlayback(argsWithProg []string, preLoop bool) (Ctrl, metadata, chan bool) {
+	file, fileExtension, fileNameRead, streamerRead, formatRead := readAudio(argsWithProg[1])
 	playInstance := new(Ctrl)
+	metadataInstance := new(metadata)
 	playInstance.fileName = fileNameRead
+	playInstance.fileExtension = fileExtension
 	playInstance.Streamer = streamerRead
 	playInstance.Format = formatRead
 
-	// Checks if to keep looping from previous instance
+	file, fileErr := os.Open(argsWithProg[1])
+	check(fileErr)
+
+	switch fileExtension {
+	//Separate metadata handling for .wav as tag package doesn't support wav
+	//Use go-audio wav package instead
+	//Use metadataInstance to contain either
+	case ".wav":
+		metadataInstance.WavMetadata = wavmeta.NewDecoder(file)
+		metadataInstance.WavMetadata.ReadMetadata()
+		if metadataInstance.WavMetadata.Metadata == nil {
+			panic("FATAL: Couldn't read song metadata")
+		}
+	default:
+		metadataInstance.nonWavMetadata = readNonWavMetadata(*file)
+	}
+
 	playInstance.Paused = false
 
+	// Checks if to keep looping from previous instance
 	playInstance.Loop = preLoop
-	return *playInstance, playAudio(*playInstance)
+	return *playInstance, *metadataInstance, playAudio(*playInstance, *metadataInstance)
 }
 
-func readInput(playInstance Ctrl, format beep.Format, done chan bool, argsWithProg []string) Ctrl {
+func readInput(playInstance Ctrl, metadataInstance metadata, format beep.Format, done chan bool, argsWithProg []string) Ctrl {
 	ch := make(chan string)
 	go func(ch chan string) {
 		reader := bufio.NewReader(os.Stdin)
@@ -145,9 +201,9 @@ stdinloop:
 						speaker.Unlock()
 						playInstance.Paused = false
 					}
-					printPlaybackStatus(playInstance)
+					printPlaybackStatus(playInstance, metadataInstance)
 				} else {
-					printPlaybackStatus(playInstance)
+					printPlaybackStatus(playInstance, metadataInstance)
 				}
 				if strings.TrimSpace(stdin) == "2" {
 					switch playInstance.Loop {
@@ -156,24 +212,23 @@ stdinloop:
 					case true:
 						playInstance.Loop = false
 					}
-					printPlaybackStatus(playInstance)
+					printPlaybackStatus(playInstance, metadataInstance)
 				} else {
-					printPlaybackStatus(playInstance)
+					printPlaybackStatus(playInstance, metadataInstance)
 				}
 			}
 		case <-time.After(1 * time.Second):
-			// Do something when there is nothing read from stdin
 			select {
 			case ok := <-done:
 				if ok {
-					// Return updated playInstance
+					// Return updated playInstance for possible next instance if looping
 					return playInstance
 				} else {
 					log.Fatal("Channel closed.")
 				}
 			default:
 				if !playInstance.Paused {
-					printPlaybackStatus(playInstance)
+					printPlaybackStatus(playInstance, metadataInstance)
 				}
 			}
 
@@ -183,7 +238,7 @@ stdinloop:
 }
 
 func playbackLoop(argsWithProg []string, preLoop bool) Ctrl {
-	playInstance, done := startPlayback(argsWithProg, preLoop)
+	playInstance, metadataInstance, done := startPlayback(argsWithProg, preLoop)
 
 	defer playInstance.Streamer.Close()
 	for {
@@ -192,10 +247,10 @@ func playbackLoop(argsWithProg []string, preLoop bool) Ctrl {
 		default:
 			switch playInstance.Paused {
 			case false:
-				playInstance = readInput(playInstance, playInstance.Format, done, argsWithProg)
+				playInstance = readInput(playInstance, metadataInstance, playInstance.Format, done, argsWithProg)
 				return playInstance
 			case true:
-				playInstance = readInput(playInstance, playInstance.Format, done, argsWithProg)
+				playInstance = readInput(playInstance, metadataInstance, playInstance.Format, done, argsWithProg)
 				return playInstance
 			default:
 			}
@@ -205,11 +260,11 @@ func playbackLoop(argsWithProg []string, preLoop bool) Ctrl {
 
 func main() {
 	argsWithProg := os.Args
-	//argsWithoutProg := os.Args[1:]
 
 	if len(argsWithProg) == 1 || len(argsWithProg) > 2 {
 		log.Fatal("Improper arguments: ./go-music-player.exe [file]")
 	}
+
 	playInstance := playbackLoop(argsWithProg, false)
 	for playInstance.Loop {
 		playInstance = playbackLoop(argsWithProg, true)
--- go.mod
+++ go.mod
@@ -1,15 +1,22 @@
 module go-audio-player
 
-go 1.23
+go 1.21.5
 
 require (
+	github.com/dhowden/tag v0.0.0-20230630033851-978a0926ee25 // indirect
 	github.com/ebitengine/oto/v3 v3.1.0 // indirect
 	github.com/ebitengine/purego v0.5.1 // indirect
+	github.com/go-audio/audio v1.0.0 // indirect
+	github.com/go-audio/riff v1.0.0 // indirect
+	github.com/go-audio/wav v1.1.0 // indirect
 	github.com/gopxl/beep v1.3.0 // indirect
 	github.com/hajimehoshi/go-mp3 v0.3.4 // indirect
 	github.com/icza/bitio v1.1.0 // indirect
+	github.com/jfreymuth/oggvorbis v1.0.5 // indirect
+	github.com/jfreymuth/vorbis v1.0.2 // indirect
 	github.com/mewkiz/flac v1.0.10 // indirect
 	github.com/mewkiz/pkg v0.0.0-20231012081350-95d6616c5403 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/term v0.13.0 // indirect
 )
